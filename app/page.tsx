7db56c335275a8e25c67ba6be64c16d479ac0a03
ryotarofr <ryoryo.fr0608@icloud.com>
2023-11-19 23:05:09 +0900

    component: corousel coed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import CarouselDemo from '@/components/carousel'
 import ToggleThemeButton from '@/components/ui/toggle-theme'
+
 export default function Home() {
   return (
-    <div>
+    <div className='w-full flex justify-center'>
       <ToggleThemeButton />
+      <CarouselDemo />
     </div>
   )
 }
