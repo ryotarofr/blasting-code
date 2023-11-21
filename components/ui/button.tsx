0473ad13869625bd2a0915ac00025e8654a1dbff
ryotarofr <ryoryo.fr0608@icloud.com>
2023-11-21 11:39:55 +0900

    💄 change ui variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
           "border border-input hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "",
         link: "underline-offset-4 hover:underline text-primary",
       },
       size: {
